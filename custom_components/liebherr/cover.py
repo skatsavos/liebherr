9c7c41a2bb9f14df09e8dce50665231790e13eba
Spyros Katsavos <skatsavos@hotmail.com>
2025-07-07 21:20:15 +0300

    Revert "Added debounce timer to improve transitions"
    This reverts commit c878ad452435bc07632b0e14ef66756d7fd0bfe2.

--- custom_components/liebherr/cover.py
+++ custom_components/liebherr/cover.py
@@ -1,9 +1,7 @@
-"""Support for Liebherr autodoor devices with debounce logic."""
+"""Support for Liebherr autodoor devices."""
 
 import asyncio
 import logging
-from datetime import datetime, timedelta
-
 from homeassistant.components.cover import CoverEntity, CoverEntityFeature
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -13,8 +11,6 @@ from .const import DOMAIN
 from .models import AutoDoorControl
 
 _LOGGER = logging.getLogger(__name__)
-
-DEBOUNCE_SECONDS = 5  # time to wait before confirming final door state
 
 
 async def async_setup_entry(
@@ -41,7 +37,7 @@ async def async_setup_entry(
 
 
 class LiebherrCover(CoverEntity):
-    """Representation of a Liebherr auto door cover with debounce."""
+    """Representation of a Liebherr auto door cover."""
 
     def __init__(self, api, coordinator, appliance, control) -> None:
         """Initialize the cover entity."""
@@ -57,12 +53,6 @@ class LiebherrCover(CoverEntity):
         self._attr_unique_id = f"{self._device_id}_{self._identifier}"
         self._attr_device_class = "door"
         self._attr_supported_features = CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE
-
-        # For debounce:
-        self._last_state = None
-        self._last_state_change = None
-        self._debounce_task = None
-        self._confirmed_state = STATE_UNKNOWN
 
     @property
     def device_info(self):
@@ -89,58 +79,34 @@ class LiebherrCover(CoverEntity):
 
         return None
 
-    async def _debounce_state(self, new_state):
-        """Debounce door state changes to avoid flickering."""
-        # Cancel existing debounce task if any
-        if self._debounce_task and not self._debounce_task.done():
-            self._debounce_task.cancel()
-
-        # If door is MOVING, update immediately but do not confirm
-        if new_state == "MOVING":
-            self._confirmed_state = STATE_OPENING
-            self.async_write_ha_state()
-            return
-
-        # If state changed from last confirmed state, start debounce delay
-        if new_state != self._confirmed_state:
-            self._last_state = new_state
-            self._last_state_change = datetime.now()
-
-            async def wait_and_confirm():
-                try:
-                    await asyncio.sleep(DEBOUNCE_SECONDS)
-                    # After waiting, confirm the new state
-                    self._confirmed_state = (
-                        STATE_OPEN if new_state == "OPEN" else STATE_CLOSED
-                    )
-                    self.async_write_ha_state()
-                except asyncio.CancelledError:
-                    # Debounce canceled because new update arrived
-                    pass
-
-            self._debounce_task = asyncio.create_task(wait_and_confirm())
-
     @property
     def state(self):
-        """Return the current debounced state of the cover."""
-        return self._confirmed_state
+        """Return the current state of the cover."""
+        value = self._get_control_state()
+        if value == "OPEN":
+            return STATE_OPEN
+        if value == "CLOSED":
+            return STATE_CLOSED
+        if value == "MOVING":
+            return STATE_OPENING
+        return STATE_UNKNOWN
 
     @property
     def is_closed(self):
         """Return True if the cover is closed."""
-        return self._confirmed_state == STATE_CLOSED
+        return self._get_control_state() == "CLOSED"
 
     @property
     def is_open(self):
         """Return True if the cover is open."""
-        return self._confirmed_state == STATE_OPEN
+        return self._get_control_state() == "OPEN"
 
     async def async_open_cover(self, **kwargs):
         """Send command to open the cover."""
         try:
             data = AutoDoorControl(zoneId=self._control.get("zoneId"), value=True)
             await self._api.set_value(self._device_id, self._control["name"], data)
-            await asyncio.sleep(3)  # Let the door start moving
+            await asyncio.sleep(3)
         except Exception as e:
             _LOGGER.error("Failed to open door %s: %s", self._identifier, e)
         await self._coordinator.async_request_refresh()
@@ -150,16 +116,7 @@ class LiebherrCover(CoverEntity):
         try:
             data = AutoDoorControl(zoneId=self._control.get("zoneId"), value=False)
             await self._api.set_value(self._device_id, self._control["name"], data)
-            await asyncio.sleep(3)  # Let the door start moving
+            await asyncio.sleep(3)
         except Exception as e:
             _LOGGER.error("Failed to close door %s: %s", self._identifier, e)
         await self._coordinator.async_request_refresh()
-
-    async def async_update(self):
-        """Called by coordinator on data update; update state with debounce."""
-        raw_state = self._get_control_state()
-        if raw_state is None:
-            self._confirmed_state = STATE_UNKNOWN
-            self.async_write_ha_state()
-            return
-        await self._debounce_state(raw_state)
